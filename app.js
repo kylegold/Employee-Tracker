89fb49d6757e3036c730ae7da1a98a7e1b33b92f
kyle <kyle@kyle.nyc>
2020-11-02 22:27:00 -0500

    added user interaction questions and user inquirer prompt

--- app.js
+++ app.js
@@ -14,3 +14,28 @@ const connection = mysql.createConnection({
     database: "employeetracker_db"
 });
 
+// data
+const userInteraction = [
+    {
+        name: "userInteraction",
+        type: "list",
+        message: "Please choose a user interaction",
+        choices: [
+            "addDepartment", 
+            "addRole", 
+            "addEmployee",
+            "viewDepartment",
+            "viewRole",
+            "viewEmployee",
+            "updateEmployeeRole"
+        ]
+    }
+]
+
+// Prompt user to add new.. departments, roles, employees
+// Prompt user to view.. departments, roles, employees
+// Prompt user to update employee role
+inquirer.prompt(userInteraction)
+.then(choice => {
+    console.log(choice)
+})
